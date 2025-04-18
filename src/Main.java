876bf8aff23508272c48ca76fad5e67087dd4c4e
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-18 15:37:41 +0700

    add

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+     Student s1=new Student(1,24,7);
+        Student s2=new Student(2,26,9);
+        Student s3=new Student(3,23,5);
+        ArrayList<Student> list=new ArrayList<Student>();
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+        Comparator<Student> byAge =new Comparator<Student>() {
+            public int compare(Student o1, Student o2) {
+                return o1.getAge()-o2.getAge();
+            }
+        };
+        list.sort(byAge);
+        System.out.println(list);
     }
 }
